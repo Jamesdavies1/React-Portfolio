b6a3ffaa4e6f1a7cea223111e7f74271fd3346be
james davies <james.davies92@icloud.com>
2020-03-29 15:07:10 +0100

    pulled files over from old folder

---
+++ react-portfolio/src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "../components/navigation/navigation";
+
+function Home () {
+    return (
+        <div>
+            <Navigation/>
+     
+        </div>
+    )
+}
+
+
+
+export default Home;
